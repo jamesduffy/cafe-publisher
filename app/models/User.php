22d7bc5bdfe2e2fc7fa942cf191d83ce5e9a4d19
James Duffy <james@cafeduff.com>
2014-12-17 23:04:29 -0800

    more stripe updates

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,14 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Laravel\Cashier\BillableTrait;
+use Laravel\Cashier\BillableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface, BillableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, BillableTrait;
+
+    protected $dates = ['trial_ends_at', 'subscription_ends_at'];
 
 	/**
 	 * The database table used by the model.
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -70,4 +70,6 @@ require $framework.'/Illuminate/Foundation/start.php';
 |
 */
 
+User::setStripeKey(Config::get('app.stripe'));
+
 return $app;
